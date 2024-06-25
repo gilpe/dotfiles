f2d5b4492954cb3b02e7ca2acc72fa7978c048f6
Gilpe <javier.gil.perez@outlook.es>
2024-06-25 21:19:50 +0200

    Install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Start post-configuration..."
+pacman -Syd
+#pacman -S --noconfirm stow zsh alacritty firefox neofetch ttf-jetbrains-mono-nerd
+#chsh gilpe
+#git clone https://aur.archlinux.org/yay.git
+echo "End post-configuration..."
