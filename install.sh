95474cb9914c601d8fdf74cfe4872e001d2575f8
Gilpe <javier.gil.perez@outlook.es>
2024-06-26 21:03:20 +0200

    recursive remove  for yay folder

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Updating packages..."
-pacman -Syd
+pacman -Syu
 
 echo "Installing new packages..."
 pacman -S --noconfirm --needed git zsh alacritty stow firefox fastfetch ttf-jetbrains-mono-nerd
@@ -14,7 +14,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ..
-rm yay
+rm yay -r
 
 echo "Install AUR packages"
 yay -S visual-studio-code-bin 
