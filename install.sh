bd5ce00d6b983eb339a6d6caf034c2a3591bda9d
Gilpe <javier.gil.perez@outlook.es>
2024-06-27 14:37:21 +0200

    yay repo path fix

--- install.sh
+++ install.sh
@@ -7,18 +7,17 @@ echo -e "\n INSTALLING PRE-REQS \n"
 sudo pacman -S --noconfirm --needed git
 
 echo -e "\n INSTALLING AUR HELPER \n"
-cd ~/Downloads
-git clone https://aur.archlinux.org/yay.git
-cd ~/Downloads/yay
+git clone https://aur.archlinux.org/yay.git ~/.yay
+cd ~/.yay
 makepkg -si
 cd ~
-rm ~/Downloads/yay -fr
+rm ~/.yay -fr
 
 echo -e "\n INSTALLING AUR PACKAGES \n"
 yay -S --noconfirm --needed zsh alacritty stow visual-studio-code-bin firefox fastfetch ttf-jetbrains-mono-nerd 
 
 echo -e "\n CHANGING SHELL \n"
-chsh -s /bin/zsh
+sudo chsh -s /bin/zsh
 
 echo -e "\n STOWING FILES \n"
 git clone https://github.com/gilpe/dotfiles.git ~/.dotfiles
