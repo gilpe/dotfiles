99d0c7abb163653fa03dd8dec8f87647424dc16c
Gilpe <javier.gil.perez@outlook.es>
2024-07-26 11:28:33 +0200

    C# parser and overall update

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return {
   opts = function(_, opts)
     -- add tsx and treesitter
     vim.list_extend(opts.ensure_installed, {
-      "csharp",
+      "c_sharp",
       "gdscript",
       "godot_resource",
       "toml",
