62f67fd5531aaff9a252bf4a055587a4786045e5
Gilpe <javier.gil.perez@outlook.es>
2025-01-03 20:52:51 +0100

    hyprland basic config

--- install.sh
+++ install.sh
@@ -20,13 +20,14 @@ echo -e "\n ==> INSTALLING PACKAGES \n"
 sudo pacman -S --noconfirm --needed zsh stow fastfetch ghostty yazi zoxide fzf btop
 yay -S --noconfirm --needed oh-my-posh
 # Windows manager
-sudo pacman -S --noconfirm --needed hyprland polkit-kde-agent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland waybar dunst grim slurp wofi
+sudo pacman -S --noconfirm --needed hyprland polkit-kde-agent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland hyprlock waybar dunst grim slurp wofi
+yay -S --noconfirm --needed hyprshot hypridle
 # Development
 sudo pacman -S --noconfirm --needed git neovim dotnet-runtime github-cli
 dotnet tool install --global Chickensoft.GodotEnv
 yay -S --noconfirm --needed rider
 # Fonts
-sudo pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd
+sudo pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd ttf-font-awesome
 # Utils
 sudo pacman -S --noconfirm --needed firefox obsidian
 
