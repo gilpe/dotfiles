be4f53cd009dc4513e512213e0dc0b3609ce1f78
Gilpe <javier.gil.perez@outlook.es>
2025-10-05 20:34:38 +0200

    restore lazyvim config

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,41 +1,8 @@
-local autocmd = vim.api.nvim_create_autocmd
-
--- Highligthing while copying
-autocmd("TextYankPost", {
-  desc = "Highlight on yank",
-  group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank { higroup = "IncSearch", timeout = 200 }
-  end,
-})
-
--- Remove whitespaces while saving
-autocmd("BufWritePre", {
-  desc = "Remove trailing whitespace on save",
-  pattern = "*",
-  command = [[%s/\s\+$//e]],
-})
-
--- Restore prompt to the latest position while opening
-autocmd("BufReadPost", {
-  desc = "Go to last loc when opening a buffer",
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, [["]])
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
-})
-
--- Update buffer when changes are made externally
-autocmd({ "FocusGained", "BufEnter" }, {
-  desc = "Reload file if changed outside",
-  command = "checktime",
-})
-
--- Autoformat on certain files
-autocmd("FileType", {
-  pattern = "gitcommit",
-  command = "setlocal spell",
-})
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,34 +1,3 @@
-local opt = vim.opt
-
--- Line numbers
-opt.number = true
-opt.relativenumber = false
-
--- Indentation
-opt.expandtab = true
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.softtabstop = 4
-opt.smartindent = true
-
--- Search
-opt.hlsearch = true
-opt.incsearch = true
-opt.ignorecase = true
-opt.smartcase = true
-
--- UI
-opt.cursorline = false
-opt.scrolloff = 8
-opt.signcolumn = "yes"
-opt.splitright = true
-opt.splitbelow = true
-opt.laststatus = 2
-opt.termguicolors = true
-
--- Files
-opt.undofile = true
-
--- Clipboard
-opt.clipboard = "unnamedplus"
-
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
